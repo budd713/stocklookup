bceeb1ef0b383bfb9faf6967e95149a73d776c70
Alexander Buddenbaum <budd713@gmail.com>
2020-09-20 19:26:09 +0800

    making sure its updated

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 // Stock Market Portfolio App by Alex Buddenbaum
-
 const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
+const request = require('request');
 
 const PORT = process.env.PORT || 5000;
+
+
+// API KEY pk_48ed691da2664891a33c171019d0bcb
+request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_48ed691da2664891a33c171019d0bcb', { json: true}, (err, res, body) => { 
+	if (err) {return console.log(err);}
+	if (res.statusCode === 200){
+		console.log(body);		
+	};
+});
+
+
 
 // Set Handlebars Middleware
 app.engine('handlebars', exphbs());
@@ -19,6 +30,11 @@ app.get('/', function (req, res) {
 		stuff: otherstuff
 	});
 })
+
+// create about page route
+app.get('/about.html', function (req, res) {
+	res.render('about'); 
+});
 
 // Set static folder 
 app.use(express.static(path.join(__dirname, 'public')));
