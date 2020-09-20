77f135473cae29b6647f6736ac7f35ae1d8914cd
Alexander Buddenbaum <budd713@gmail.com>
2020-09-21 00:29:03 +0800

    -first working draft of home and about pages.

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
 const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
 
 
+// use body parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
 
-// API KEY pk_48ed691da2664891a33c171019d0bcb
+
+// API KEY pk_48ed691da2664891a33c171019d0bcb4
 // create call_api function
-function call_api(finishedAPI) {
-	request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_48ed691da2664891a33c171019d0bcb', { json: true }, (err, res, body) => { 
+function call_api(finishedAPI, ticker) {
+	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_48ed691da2664891a33c171019d0bcb4', { json: true }, (err, res, body) => { 
 	if (err) {return console.log(err);}
 	if (res.statusCode === 200){
 		// console.log(body);		
@@ -29,13 +33,25 @@ app.set('view engine', 'handlebars');
 
 const otherstuff = "hello there, this is other stuff!";
 
-// Set handlebar routes
+// Set handlebar index GET route
 app.get('/', function (req, res) {
 	call_api(function(doneAPI) {
 			res.render('home', {
 			stock: doneAPI
 		});
-	});
+	}, "fb");
+	
+});
+
+// Set handlebar index POST route
+app.post('/', function (req, res) {
+	call_api(function(doneAPI) {
+			// posted_stuff = req.body.stock_ticker;
+			res.render('home', {
+			stock: doneAPI,
+
+		});
+	}, req.body.stock_ticker);
 	
 });
 
